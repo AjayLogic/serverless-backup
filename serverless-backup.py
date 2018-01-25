24cd5ae95791fc4e7a8e73ffabf183d075aa08b3
Mystique <13oct08@quantumfoam.uni.cc>
2018-01-25 10:57:06 +0530

    Added changes to include tags for "Name", "Description" Added Retention days as variable
    Signed-off-by: Mystique <13oct08@quantumfoam.uni.cc>

--- serverless-backup.py
+++ serverless-backup.py
@@ -1,4 +1,7 @@
-import boto3
+import boto3  
+import collections  
+import datetime
+
 
 # Set the global variables
 globalVars  = {}
@@ -6,40 +9,82 @@ globalVars['Owner']                 = "Miztiik"
 globalVars['Environment']           = "Test"
 globalVars['REGION_NAME']           = "ap-south-1"
 globalVars['tagName']               = "Valaxy-Serverless-Automated-Backup"
-globalVars['findNeedle']            = ["backup", "Backup"]
+globalVars['findNeedle']            = "BackUp"
+globalVars['RetentionTag']          = "DeleteOn"
+globalVars['RetentionInDays']       = "70"
 
-ec2Client = boto3.client('ec2')
+#Please mention your region name
+ec = boto3.client('ec2', region_name='ap-south-1')
 
+def backup_bot():
 
-def lambda_handler(event, context):
-    reservations = ec2Client.describe_instances( Filters=[ {'Name': 'tag-key', 'Values': globalVars['findNeedle'] }] ).get('Reservations', [] )
+    snapsCreated = { 'Snapshots':[], }
 
-    print "Found {0} instances that need backing up".format( len(reservations) )
+    filters = [
+        {'Name': 'tag-key', 'Values': [ globalVars['findNeedle'] ]},
+        {'Name': 'tag-value', 'Values': ['Yes']},
+    ]
 
-    instances = sum([[i for i in r['Instances']] for r in reservations], [])
+    reservations = ec.describe_instances( Filters=filters ).get( 'Reservations', [] )
 
-    backupStatus=[]
+    instances = sum(
+        [
+            [i for i in r['Instances']]
+            for r in reservations
+        ], [])
+
+    # print "Number of the Instances : %d" % len(instances)
+    snapsCreated['InstanceCount']=len(instances)
+
+    to_tag = collections.defaultdict(list)
+
+    # Iterate for all Instances in the Region
     for instance in instances:
+        try:
+            retention_days = [
+                int(t.get('Value')) for t in instance['Tags']
+                if t['Key'] == 'Retention'][0]
+        except IndexError:
+            retention_days = int(globalVars['RetentionInDays'])
+
+        # Get all the Block Device Mappings
         for dev in instance['BlockDeviceMappings']:
             if dev.get('Ebs', None) is None:
                 continue
             vol_id = dev['Ebs']['VolumeId']
 
-            print 'Found EBS volume {0} on instance {1}'.format( vol_id, instance['InstanceId'])
+            #To get all the instance tags deatils
+            for tag in instance['Tags']:
+                DescriptionTxt = ''
+                if tag['Key'] == 'Name' :
+                    DescriptionTxt = tag['Value']
+            
+            snap = ec.create_snapshot( VolumeId=vol_id, Description=DescriptionTxt )
 
-            snapShot = ec2Client.create_snapshot( VolumeId=vol_id )
+            to_tag[retention_days].append(snap['SnapshotId'])
 
-            # Add tags to the snapshot
-            ec2Client.create_tags(Resources=[snapShot['SnapshotId'], ],
-                                  Tags=[{'Key': 'Name', 'Value': globalVars['tagName']}, ]
-                                  )
+            # Tag all the snaps that were created today with Deletion Date
+            for retention_days in to_tag.keys():
+                delete_date = datetime.date.today() + datetime.timedelta(days=retention_days)
+                # to mention the current date formet
+                delete_fmt = delete_date.strftime('%Y-%m-%d')
+                # below code is create the name and current date as instance name
+                ec.create_tags(
+                                Resources=to_tag[retention_days],
+                                Tags=[
+                                        {'Key': globalVars['RetentionTag'], 'Value': delete_fmt},
+                                        {'Key': 'Name', 'Value': snap['Description'] },
+                                    ]
+                            ) 
+                snapsCreated['Snapshots'].append({ 'SnapshotId':snap['SnapshotId'], 'VolumeId' : vol_id, 'InstanceId' : instance['InstanceId'], 'DeleteOn': delete_fmt })
+        
+        to_tag.clear()
+  
+    return snapsCreated
 
-            # Append to backup status update list
-            backupStatus.append({'InstanceId':instance['InstanceId'],
-                                 'VolumeId': vol_id,
-                                 'SnapshotId':snapShot['SnapshotId'],
-                                 'State':snapShot['State']
-                                }
-                               )
-    # Return the status of the snapshots triggered by lambda as list
-    return backupStatus
+
+def lambda_handler(event, context):
+    return backup_bot()
+
+if __name__ == '__main__':
+    lambda_handler(None, None)
